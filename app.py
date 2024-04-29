14ccadd0c03bfb9a68e64153d9c4206e888dd671
Man-Chun Yu <brian0821yu@gmail.com>
2024-04-29 16:10:13 +0800

    Set up Flask app boilerpates.

---
+++ app.py
@@ -0,0 +1,13 @@
+import datetime
+import pytz
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
